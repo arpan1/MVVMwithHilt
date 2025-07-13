dbd3f334734cb2b39b90ec07b7a187ee941fbd10
arpanbhatia <arpanbhatia.ec@gmail.com>
2025-07-13 22:23:29 +0530

    Commit for Git Merge

--- app/src/main/java/com/example/androidarchitecturedesign/ui/view/NameViewModel.kt
+++ app/src/main/java/com/example/androidarchitecturedesign/ui/view/NameViewModel.kt
@@ -20,7 +20,10 @@ class NameViewModel @Inject constructor(private  val userRepo : UserRepository) 
     private val _state = MutableStateFlow<UiState<UserResponse>>(UiState.Idle)
     val state: StateFlow<UiState<UserResponse>> = _state.asStateFlow()
 
+    fun fetchUsers2(){
 
+
+    }
     fun fetchUsers(){
         viewModelScope.launch {
             _state.value = UiState.Loading
